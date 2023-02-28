d7b3930ae85699f0dcacadbf737f5d9025dbda08
Alex Kaszynski <akascap@gmail.com>
2023-02-28 22:58:23 +0000

    use qtbot

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,8 @@
-import sys
-
-from PySide6.QtWidgets import QApplication
 import pytest
 
 from ansys.tools.installer import AnsysPythonInstaller
 
 
 @pytest.fixture()
-def gui():
-    if not QApplication.instance():
-        app = QApplication(sys.argv)
-    else:
-        app = QApplication.instance([])
+def gui(qtbot):
     return AnsysPythonInstaller(show=False)
