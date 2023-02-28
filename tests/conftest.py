fa9d0ca6dccec1bb9008393ebafaf5f0dc599ce1
Alex Kaszynski <akascap@gmail.com>
2023-02-28 21:27:59 +0000

    add initial CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,16 @@
+import sys
+
 from PySide6.QtWidgets import QApplication
 import pytest
 
 from ansys.tools.installer import AnsysPythonInstaller
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def gui():
-    app = QApplication(["-platform", "offscreen"])
+    # ["--platform", "offscreen"]
+    if not QApplication.instance():
+        app = QApplication(sys.argv)
+    else:
+        app = QApplication.instance()
     return AnsysPythonInstaller(show=False)
