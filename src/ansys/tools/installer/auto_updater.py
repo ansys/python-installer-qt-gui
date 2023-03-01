d576da49ec30ab98248e201bc4a237406b0b12ce
Alex Kaszynski <akascap@gmail.com>
2023-03-01 11:19:57 -0800

    test add read only token

---
+++ src/ansys/tools/installer/auto_updater.py
@@ -0,0 +1,3 @@
+# Readonly on this repo
+# This repository will be released to the public, there's no issue with this token.
+PAT = "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"
