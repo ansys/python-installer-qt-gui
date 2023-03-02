a57726095d05d61a727ca2c6651a5b0db37f7af8
RobPasMue <37798125+RobPasMue@users.noreply.github.com>
2023-03-02 06:47:09 -0800

    Updating pip prior to installing anything

--- src/ansys/tools/installer/installed_table.py
+++ src/ansys/tools/installer/installed_table.py
@@ -186,44 +186,60 @@ class InstalledTab(QtWidgets.QWidget):
         """Launch spyder IDE."""
         # handle errors
         error_msg = "pip install spyder && spyder || echo Failed to launch. Try reinstalling spyder with pip install spyder --force-reinstall"
+        self._update_pck_mnger()
         self.launch_cmd(f"spyder || {error_msg}")
 
     def launch_jupyterlab(self):
         """Launch Jupyterlab."""
         # handle errors
         error_msg = "pip install jupyterlab && python -m jupyter lab || echo Failed to launch. Try reinstalling jupyterlab with pip install jupyterlab --force-reinstall"
+        self._update_pck_mnger()
         self.launch_cmd(f"python -m jupyter lab || {error_msg}")
 
     def launch_jupyter_notebook(self):
         """Launch Jupyter Notebook."""
         # handle errors
         error_msg = "pip install jupyter && python -m jupyter notebook || echo Failed to launch. Try reinstalling jupyter with pip install jupyter --force-reinstall"
+        self._update_pck_mnger()
         self.launch_cmd(f"python -m jupyter notebook || {error_msg}")
 
     def install_defaults(self):
         """Install Python default packages."""
-        cmd = "pip install numpy pandas scipy scikit-learn matplotlib & timeout 3 & exit || echo Failed to install default Python packages. Try reinstalling it with pip install numpy pandas scipy scikit-learn matplotlib --force-reinstall"
+        cmd = "pip install numpy pandas scipy scikit-learn matplotlib && timeout 3 && exit || echo Failed to install default Python packages. Try reinstalling it with pip install numpy pandas scipy scikit-learn matplotlib --force-reinstall"
+        self._update_pck_mnger()
         self.launch_cmd(cmd)
 
     def install_pyansys(self):
         """Install PyAnsys metapackage."""
-        cmd = "pip install pyansys^>=2023 & timeout 3 & exit || echo Failed to install PyAnsys metapackage. Try reinstalling it with pip install pyansys^>=2023 --force-reinstall"
+        cmd = "pip install pyansys^>=2023 && timeout 3 && exit || echo Failed to install PyAnsys metapackage. Try reinstalling it with pip install pyansys^>=2023 --force-reinstall"
+        self._update_pck_mnger()
         self.launch_cmd(cmd)
 
     def list_packages(self):
         """List installed Python packages."""
         self.launch_cmd("pip list")
 
-    def launch_cmd(self, extra=""):
+    def _update_pck_mnger(self):
+        """Update package manager if needed."""
+        cmd = ""
+        if "Python" in self.table.active_version:
+            cmd = "python -m pip install -U pip & exit"
+        elif "Conda" in self.table.active_version:
+            cmd = "conda update conda & exit"
+        self.launch_cmd(cmd, True)
+
+    def launch_cmd(self, extra="", minimized_window=False):
         """Run a command in a new command prompt.
 
         Parameters
         ----------
         extra : str, default: ""
             Any additional command(s).
-
+        minimized_window : bool, default: False
+            Whether the window should run minimized or not.
         """
         py_path = self.table.active_path
+        min_win = "/w /min" if minimized_window else ""
         if "Python" in self.table.active_version:
             scripts_path = os.path.join(py_path, "Scripts")
             new_path = f"{py_path};{scripts_path};%PATH%"
@@ -233,8 +249,9 @@ class InstalledTab(QtWidgets.QWidget):
             else:
                 cmd = f"& echo Python set to {py_path}"
 
-            subprocess.Popen(
-                f'start cmd /K "set PATH={new_path}&cd %userprofile%{cmd}"', shell=True
+            subprocess.call(
+                f'start {min_win} cmd /K "set PATH={new_path}&cd %userprofile%{cmd}"',
+                shell=True,
             )
         else:  # probably conda
             if extra:
@@ -243,7 +260,7 @@ class InstalledTab(QtWidgets.QWidget):
                 cmd = f"& {extra}"
             else:
                 cmd = f"& echo Activating conda forge at path {py_path}"
-            subprocess.Popen(
-                f'start cmd /K "{py_path}\\Scripts\\activate.bat {py_path}&cd %userprofile%{cmd}"',
+            subprocess.call(
+                f'start {min_win} cmd /K "{py_path}\\Scripts\\activate.bat {py_path}&cd %userprofile%{cmd}"',
                 shell=True,
             )
