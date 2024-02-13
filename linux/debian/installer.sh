04570a351bafe126279960ebc8ede0f363c5040f
Sakthi Prakash R <151930473+anssakthi@users.noreply.github.com>
2024-02-13 18:46:19 +0530

    Alter script

--- linux/debian/installer.sh
+++ linux/debian/installer.sh
@@ -42,18 +42,17 @@ else
         sudo -v >/dev/null 2>&1
         root_check=$?
         if [ $root_check -eq 0 ]; then
-            install_script="sudo apt-get update -y; sudo apt-get install "
+            install_script="sudo apt-get update -y; "
             install_zlib=false
             for x in "${missing_deps[@]}"; do
                 if [ $x == "zlib" ]; then
                     install_zlib=true
                 else
-                    install_script="$install_script $x"
+                    install_script="$install_script sudo apt-get install $x -y;"
                 fi
             done
-            install_script="$install_script -y"
             if [ $install_zlib = true ]; then
-                install_script="$install_script; mkdir ansys-prereq; cd ansys-prereq; wget https://zlib.net/current/zlib.tar.gz; tar xvzf zlib.tar.gz; cd zlib-*; make clean; ./configure; make; sudo make install; cd ../..; rm -rf ansys-prereq;"
+                install_script="$install_script rm -rf ansys-prereq/ ; mkdir -p ansys-prereq; cd ansys-prereq; wget https://zlib.net/current/zlib.tar.gz; tar xvzf zlib.tar.gz; cd zlib-*; make clean; ./configure; make; sudo make install; cd ../..; rm -rf ansys-prereq;"
             fi
             eval $install_script
             dpkg -x ./ansys_python_manager.deb ${HOME}/.local
