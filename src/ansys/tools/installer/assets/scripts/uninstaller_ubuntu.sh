036a6ae7917a45e92426ca3374e0a170f677449a
Sakthi Prakash R <151930473+anssakthi@users.noreply.github.com>
2024-02-29 12:11:44 +0530

    Update uninstaller_ubuntu.sh

--- src/ansys/tools/installer/assets/scripts/uninstaller_ubuntu.sh
+++ src/ansys/tools/installer/assets/scripts/uninstaller_ubuntu.sh
@@ -1,3 +1,6 @@
+echo "Uninstalling Ansys Python Manager......."
+sleep 3
+
 rm -rf ${HOME}/.local/share/icons/pyansys_icon.svg
 rm -rf ${HOME}/.local/share/applications/ansys_python_manager.desktop
 rm -rf ${HOME}/.local/usr/share/doc/ansys-python-manager
