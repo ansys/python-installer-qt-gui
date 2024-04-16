6a6f79a94e68dadf306764256c67c53e90ec6771
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-04-16 12:06:21 +0200

    fix: pre-commit

--- scripts/update_python_versions.py
+++ scripts/update_python_versions.py
@@ -1,4 +1,4 @@
-""" Script that updates the Python versions used in the project """
+"""Script that updates the Python versions used in the project."""
 
 import os
 import re
@@ -6,12 +6,38 @@ import re
 import requests
 
 
-def is_version_string(s):
+def is_version_string(s: str) -> bool:
+    """Check if the string is in accepted version format.
+
+    Parameters
+    ----------
+    s : str
+        String to check.
+
+    Returns
+    -------
+    bool
+        True if the string is in the accepted version format, False otherwise.
+    """
     pattern = r"^\d+\.\d+\.\d+$"
     return bool(re.match(pattern, s))
 
 
-def get_latest_github_release(user, repo):
+def get_latest_github_release(user, repo) -> dict:
+    """Get the latest release of a GitHub repository.
+
+    Parameters
+    ----------
+    user : str
+        GitHub username.
+    repo : str
+        Repository name.
+
+    Returns
+    -------
+    dict
+        JSON response of the latest release.
+    """
     url = f"https://api.github.com/repos/{user}/{repo}/releases/latest"
     response = requests.get(url)
     if response.status_code == 200:
@@ -21,7 +47,8 @@ def get_latest_github_release(user, repo):
         return None
 
 
-def get_minor_version_sublist_with_greater_patch(list : list[str], current_version : str):
+def get_minor_version_sublist_with_greater_patch(list: list[str], current_version: str):
+    """Get the sublist of versions with greater patch than the current version."""
     major, minor, patch = current_version.split(".")
     major, minor, patch = int(major), int(minor), int(patch)
     sublist = [version for version in list if version.startswith(f"{major}.{minor}.")]
@@ -38,7 +65,9 @@ def get_minor_version_sublist_with_greater_patch(list : list[str], current_versi
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Path to the constants file
-CONSTANTS_FILE = os.path.join(ROOT_DIR, "src", "ansys", "tools", "installer", "constants.py")
+CONSTANTS_FILE = os.path.join(
+    ROOT_DIR, "src", "ansys", "tools", "installer", "constants.py"
+)
 
 # Parse the constants file to find the current Python versions
 # used in the project. The versions are stored in a tuple
@@ -60,8 +89,8 @@ with open(CONSTANTS_FILE, "r") as f:
 # CONDA_PYTHON_VERSION = "23.1.0-4"
 #
 
-vanilla_python_versions : dict[str, str]= {}
-conda_python_version : str = ""
+vanilla_python_versions: dict[str, str] = {}
+conda_python_version: str = ""
 
 for line in lines:
     if "VANILLA_PYTHON_VERSIONS" in line:
@@ -77,7 +106,7 @@ for line in lines[start_index:]:
         break
 
 # Parse the dictionary to get the Python versions
-for line in lines[start_index:end_index + 1]:
+for line in lines[start_index : end_index + 1]:
     if "Python" in line:
         # Extract the Python version and the version number
         python_version = line.split(":")[0].strip().replace('"', "")
@@ -123,14 +152,20 @@ for python_version_key, python_version_value in vanilla_python_versions.items():
     minor_version = ".".join(python_version_value.split(".")[:2])
 
     # Get the patch versions available
-    patch_versions = get_minor_version_sublist_with_greater_patch(ftp_versions, python_version_value)
+    patch_versions = get_minor_version_sublist_with_greater_patch(
+        ftp_versions, python_version_value
+    )
 
     # Check if the patch versions contain the executable
     new_patch_version = None
     for patch_version in patch_versions:
         # Check if the executable exists
-        response_1 = requests.get(f"{PYTHON_FTP}/{patch_version}/Python-{patch_version}.tar.xz")
-        response_2 = requests.get(f"{PYTHON_FTP}/{patch_version}/python-{patch_version}-amd64.exe")
+        response_1 = requests.get(
+            f"{PYTHON_FTP}/{patch_version}/Python-{patch_version}.tar.xz"
+        )
+        response_2 = requests.get(
+            f"{PYTHON_FTP}/{patch_version}/python-{patch_version}-amd64.exe"
+        )
         if response_1.status_code == 200 and response_2.status_code == 200:
             print(f"Python {patch_version} is available for download")
             new_patch_version = patch_version
@@ -150,9 +185,9 @@ assets = latest_conda_release["assets"]
 new_conda_version = None
 count = 0
 for asset in assets:
-    if f"Miniforge3-{latest_conda_release["name"]}-Linux-x86_64.sh" == asset["name"]:
+    if f"Miniforge3-{latest_conda_release['name']}-Linux-x86_64.sh" in asset["name"]:
         count += 1
-    if f"Miniforge3-{latest_conda_release["name"]}-Windows-x86_64.exe" in asset["name"]:
+    if f"Miniforge3-{latest_conda_release['name']}-Windows-x86_64.exe" in asset["name"]:
         count += 1
     if count == 2:
         new_conda_version = latest_conda_release["name"]
