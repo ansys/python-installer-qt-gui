e242c7528cfff8f7d70be72c0515328b1f4e018d
dkunhamb <dipin.kunhambunair@ansys.com>
2025-02-18 08:33:57 -0600

    update python versions

--- src/ansys/tools/installer/constants.py
+++ src/ansys/tools/installer/constants.py
@@ -221,11 +221,10 @@ VENV_SEARCH_PATH = "venv_search_path"
 #
 
 VANILLA_PYTHON_VERSIONS = {
-    "Python 3.8": "3.8.10",
-    "Python 3.9": "3.9.13",
     "Python 3.10": "3.10.11",
     "Python 3.11": "3.11.9",
     "Python 3.12": "3.12.9",
+    "Python 3.13": "3.13.2",
 }
 
 CONDA_PYTHON_VERSION = "24.1.2-0"
